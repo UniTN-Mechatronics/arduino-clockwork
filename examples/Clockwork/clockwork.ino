1979ed6a5a66c55c9e49ca0fef4f53e0c9f3069b
Paolo Bosetti <paolo.bosetti@unitn.it>
2015-05-20 19:04:58 +0200

    Updated README and example

--- examples/Clockwork/clockwork.ino
+++ examples/Clockwork/clockwork.ino
@@ -1,10 +1,18 @@
+/*
+  Clockwork example
+  by Paolo Bosetti 2015
+*/
 #include <clockwork.h>
 
+// Warning callback. Whenever the task execution time (TET) becomes larger than
+// the allocated loop time (ALT), this function is auomatically triggered.
 void tet_warning(long t) {
   Serial.print(t);
   Serial.println(" TET warning!");
 }
 
+// Instance the clockwork with 500 ms of ALT, also setting tet_warning() as
+// warning callback
 Clockwork cw(500, tet_warning);
 
 void setup() {
@@ -12,7 +20,9 @@ void setup() {
 }
 
 void loop() {
+  // start the timing. This must be the very first call in your loop
   cw.start();
+  
   delay(400 + random(0, 110));
   Serial.print("tet=");
   Serial.print(cw.tet());
@@ -20,5 +30,8 @@ void loop() {
   Serial.print(cw.last_delay()); 
   Serial.print(" total=");
   Serial.println(cw.tet() + cw.last_delay());
+  
+  // Stop the taiming and wait until ALT ms elapsed since cw.start() call
+  // This call returns true if TET < ALT, false otherwise.
   cw.stop();
 }
